c496c9eb4afe71ef5ce7757066e2460ecb24038a
DavidGL17 <david.gonzalez@citycable.ch>
2024-04-04 16:52:41 +0200

    fix auth. works with server

--- frontend/src/app/api/auth/[...nextauth]/options.ts
+++ frontend/src/app/api/auth/[...nextauth]/options.ts
@@ -10,27 +10,16 @@ const options: NextAuthOptions = {
                 password: { label: "Password", type: "password" },
             },
             async authorize(credentials, req) {
-                // const res = await fetch("http://localhost:3001/auth/login", {
-                //     method: "POST",
-                //     headers: { "Content-Type": "application/json" },
-                //     body: JSON.stringify({
-                //         username: credentials?.email,
-                //         password: credentials?.password,
-                //     }),
-                // });
-                // const user = await res.json();
-                // if (res.ok && user) {
-                //     return user;
-                // } else {
-                //     return null;
-                // }
-
-                const user = {
-                    id: "001",
-                    email: "test",
-                    password: "test",
-                };
-                if (credentials?.email === user.email && credentials?.password === user.password) {
+                const res = await fetch("http://localhost:3001/auth/login", {
+                    method: "POST",
+                    headers: { "Content-Type": "application/json" },
+                    body: JSON.stringify({
+                        email: credentials?.email,
+                        password: credentials?.password,
+                    }),
+                });
+                const user = await res.json();
+                if (res.ok && user) {
                     return user;
                 } else {
                     return null;
