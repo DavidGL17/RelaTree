19f4aa2fd7486600fad26ad3b8e14a008f795409
DavidGL17 <david.gonzalez@citycable.ch>
2024-05-26 20:31:44 +0200

    added nextui

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -1,4 +1,4 @@
-import AuthProvider from "@/context/AuthProvider";
+import Provider from "@/context/Provider";
 import "./globals.css";
 import { Inter } from "next/font/google";
 import Navbar from "@/components/Navbar";
@@ -14,12 +14,12 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
     return (
         <html lang="en">
             <body className={inter.className}>
-                <AuthProvider>
+                <Provider>
                     <Navbar />
                     <main className="flex justify-center items-start p-6 h-[calc(100vh_-_80px)] bg-gray-900">
                         {children}
                     </main>
-                </AuthProvider>
+                </Provider>
             </body>
         </html>
     );
---
+++ frontend/src/context/Provider.tsx
@@ -0,0 +1,12 @@
+"use client";
+
+import { SessionProvider } from "next-auth/react";
+import { NextUIProvider } from "@nextui-org/react";
+
+export default function Provider({ children }: { children: React.ReactNode }) {
+    return (
+        <NextUIProvider>
+            <SessionProvider>{children}</SessionProvider>
+        </NextUIProvider>
+    );
+}
